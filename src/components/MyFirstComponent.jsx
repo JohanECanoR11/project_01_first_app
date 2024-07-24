4468fc8e9e7dd39efe271d22ec9e287f94c4b40e
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-07-23 21:56:01 -0500

    my first component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+      <div>MyFirstComponent</div>
+      <div>Hello World, I'm Johan Eduardo Cano Ruiz</div>
+      <div>27 de junio de 2024</div>
+    </>
+  )
+}
